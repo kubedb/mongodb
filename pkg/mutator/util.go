c1bfd14bdaaf6b315d23b17be858711968159dbd
the-redback <maruf@appscode.com>
2018-03-15 10:43:10 +0600

    added-all

--- pkg/mutator/util.go
+++ pkg/mutator/util.go
@@ -3,7 +3,6 @@ package mutator
 import (
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
 	cs "github.com/kubedb/apimachinery/client/clientset/versioned/typed/kubedb/v1alpha1"
-	core "k8s.io/api/core/v1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
