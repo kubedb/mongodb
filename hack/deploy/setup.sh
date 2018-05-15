8b0552406f6c2feec54d772b06a243bf8aa5ad22
the-redback <maruf@appscode.com>
2018-05-15 18:35:47 +0600

    added-all

--- hack/deploy/setup.sh
+++ hack/deploy/setup.sh
@@ -38,3 +38,8 @@ fi
 echo ""
 echo "${KUBEDB_SCRIPT}hack/deploy/kubedb.sh | bash -s -- --operator-name=mg-operator "$@""
 ${KUBEDB_SCRIPT}hack/deploy/kubedb.sh | bash -s -- --operator-name=mg-operator "$@"
+
+if [ `pwd` = "$CLI_ROOT" ]; then
+    popd
+fi
+popd
