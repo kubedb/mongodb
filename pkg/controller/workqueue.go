5dd2904a423220ecde32906af210e1fbc5e7746a
the-redback <maruf.2hin@gmail.com>
2018-08-15 18:59:05 +0600

    revendored

--- pkg/controller/workqueue.go
+++ pkg/controller/workqueue.go
@@ -3,9 +3,7 @@ package controller
 import (
 	"github.com/appscode/go/log"
 	core_util "github.com/appscode/kutil/core/v1"
-	meta_util "github.com/appscode/kutil/meta"
 	"github.com/appscode/kutil/tools/queue"
-	"github.com/golang/glog"
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
 	"github.com/kubedb/apimachinery/client/clientset/versioned/typed/kubedb/v1alpha1/util"
 )
@@ -17,34 +15,8 @@ func (c *Controller) initWatcher() {
 	c.mgInformer.AddEventHandler(queue.NewEventHandler(c.mgQueue.GetQueue(), func(old interface{}, new interface{}) bool {
 		oldObj := old.(*api.MongoDB)
 		newObj := new.(*api.MongoDB)
-		return newObj.DeletionTimestamp != nil || !mongodbEqual(oldObj, newObj)
+		return newObj.DeletionTimestamp != nil || newObj.Equal(oldObj)
 	}))
-}
-
-func mongodbEqual(old, new *api.MongoDB) bool {
-	if api.EnableStatusSubresource {
-		// At this moment, metadata.Generation is incremented only by `spec`.
-		// issue tracked: https://github.com/kubernetes/kubernetes/issues/67428
-		// So look for changes in metadata.labels as well.
-		if new.Generation <= new.Status.ObservedGeneration && meta_util.Equal(old.Labels, new.Labels) {
-			return true
-		}
-		if glog.V(log.LevelDebug) {
-			diff := meta_util.Diff(old, new)
-			glog.Infof("meta.Generation [%d] is higher than status.observedGeneration [%d] in MongoDB %s/%s with Diff: %s",
-				new.Generation, new.Status.ObservedGeneration, new.Namespace, new.Name, diff)
-		}
-		return false
-	}
-
-	if !meta_util.Equal(old.Spec, new.Spec) || !meta_util.Equal(old.Labels, new.Labels) {
-		if glog.V(log.LevelDebug) {
-			diff := meta_util.Diff(old, new)
-			glog.Infof("MongoDB %s/%s has changed. Diff: %s", new.Namespace, new.Name, diff)
-		}
-		return false
-	}
-	return true
 }
 
 func (c *Controller) runMongoDB(key string) error {
