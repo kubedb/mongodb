526b93b8fbbdbac2abf88510118404b5165fb351
the-redback <maruf.2hin@gmail.com>
2018-09-13 15:05:25 +0600

    Support Termination Policy

--- pkg/admission/validator.go
+++ pkg/admission/validator.go
@@ -215,6 +215,12 @@ func matchWithDormantDatabase(extClient kubedbv1alpha1.KubedbV1alpha1Interface, 
 	// Skip checking doNotPause
 	drmnOriginSpec.DoNotPause = originalSpec.DoNotPause
 
+	// Skip checking UpdateStrategy
+	drmnOriginSpec.UpdateStrategy = originalSpec.UpdateStrategy
+
+	// Skip checking TerminationPolicy
+	drmnOriginSpec.TerminationPolicy = originalSpec.TerminationPolicy
+
 	// Skip checking Monitoring
 	drmnOriginSpec.Monitor = originalSpec.Monitor
 
