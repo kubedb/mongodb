ec9eaaded5eceb43f5645fa790109412f7effab9
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2021-04-16 09:49:47 +0600

    revendor
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- vendor/kmodules.xyz/client-go/meta/ipv6.go
+++ vendor/kmodules.xyz/client-go/meta/ipv6.go
@@ -2,13 +2,14 @@ package meta
 
 import (
 	"context"
+	"io/ioutil"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func IPv6Enabled(kc kubernetes.Interface) (bool, error) {
+func IPv6EnabledOnCluster(kc kubernetes.Interface) (bool, error) {
 	svc, err := kc.CoreV1().Services(metav1.NamespaceDefault).Get(context.TODO(), "kubernetes", metav1.GetOptions{})
 	if err != nil {
 		return false, err
@@ -18,6 +19,17 @@ func IPv6Enabled(kc kubernetes.Interface) (bool, error) {
 		if strings.ContainsRune(ip, ':') {
 			return true, nil
 		}
+	}
+	return false, nil
+}
+
+func IPv6EnabledOnKernel() (bool, error) {
+	content, err := ioutil.ReadFile("/sys/module/ipv6/parameters/disable")
+	if err != nil {
+		return false, err
+	}
+	if strings.Contains(string(content), "0" ) {
+		return true, err
 	}
 	return false, nil
 }
